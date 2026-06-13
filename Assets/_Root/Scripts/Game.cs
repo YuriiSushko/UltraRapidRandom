d3b4924b22822c66b0d193ea1e884a9f76507919
Daniel H <ddushynska@kse.org.ua>
2026-06-13 22:48:37 +0300

    add UI manager

--- Assets/_Root/Scripts/Game.cs
+++ Assets/_Root/Scripts/Game.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
 
 public class Game : MonoBehaviour
 {
-
-
     private BoardController boardController_;
+    [SerializeField] private UIManager uiManager_;
+
     void Awake()
     {
-        boardController_ = GetComponent<BoardController>();   
+        boardController_ = GetComponent<BoardController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        if (uiManager_ != null)
+        {
+            uiManager_.UpdatePlayer1UI("P1 rule", "P1 goal");
+            uiManager_.UpdatePlayer2UI("P2 rule", "P2 goal");
+        }
+        else
+        {
+            Debug.LogError($"UIManager reference is missing on {gameObject.name}! Drag your UI Canvas or Manager object into the Inspector slot.");
+        }
     }
 }
